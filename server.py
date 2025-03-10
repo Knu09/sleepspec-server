58d7f0acf85936606fa8ecc1efa9412a9136517b
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-10 10:49:31 +0800

    add Index route

--- server.py
+++ server.py
@@ -1,7 +1,35 @@
-from flask import Flask
+from enum import Enum
+from flask import Flask, request, jsonify 
+from pathlib import Path
+
+from werkzeug.datastructures import FileStorage
 
 app = Flask(__name__)
 
-@app.route("/")
-def classify():
-    return ""
+class Class(Enum):
+    NSD = 0
+    SD = 1
+
+@app.route("/", methods=["POST"])
+def Index():
+    if "audio" not in request.files:
+        return jsonify({"error": "No audio file in request."}), 400
+
+    audio_file = request.files["audio"]
+    if audio_file.filename:
+        file_path = Path("./tmp") / audio_file.filename
+        audio_file.save(file_path)
+
+        classify(file_path)
+        
+        return jsonify({
+            "class": Class.NSD,
+            "result": "You are not sleep deprived.",
+        }), 200
+    
+    return jsonify({"error": "There was a problem saving the file"}), 500
+
+def classify(audio_path: Path) -> Class:
+    # insert classification logic here
+
+    return Class.NSD # or Class.SD
