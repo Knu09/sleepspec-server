53caf4ce62a4146c57994b442dc27b8e5ae712b1
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-10 15:48:55 +0800

    add result description

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ class SD_Class(Enum):
 class Classification:
     sd: SD_Class
     confidence_score: float
+    result: str
     # other fields here
 
 @app.route("/", methods=["POST"])
@@ -24,18 +25,27 @@ def Index():
 
     audio_file = request.files["audio"]
     if audio_file.filename:
-        file_path = Path("./tmp/uploads") / secure_filename(audio_file.filename)
+        uploads_path = 'tmp/uploads'
+        Path(uploads_path).mkdir(parents=True, exist_ok=True)
+
+        uploads = Path(uploads_path)
+
+        file_path = uploads / secure_filename(audio_file.filename)
         audio_file.save(file_path)
 
-        classification = classify(file_path)
+        clf = classify(file_path)
 
         return jsonify({
-            "class": classification.sd,
-            "result": "You are not sleep deprived.",
+            "class": clf.sd.value,
+            "result": clf.result,
         }), HTTPStatus.OK
     
     return jsonify({"error": "There was a problem saving the file"}), HTTPStatus.INTERNAL_SERVER_ERROR
 
 def classify(audio_path: Path) -> Classification:
     # insert classification logic here
-    return Classification(sd=SD_Class.NSD, confidence_score=0.826) # 82.6%
+    return Classification(
+        sd=SD_Class.NSD,
+        confidence_score=0.826, # 82.6%
+        result="You are not sleep deprived."
+    )
