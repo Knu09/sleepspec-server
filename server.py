ddaa9c9cdcb9dfbf3167adff5c5fb143b876d9df
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-10 18:39:41 +0800

    use pydub for converting m4a to wav

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
-from enum import Enum
 from werkzeug.utils import secure_filename
 from flask import Flask, request, jsonify
+from pydub import AudioSegment
 from pathlib import Path
 from http import HTTPStatus
+from enum import Enum
 from dataclasses import dataclass
 
 app = Flask(__name__)
 
+
 class SD_Class(Enum):
     NSD = 0
     SD = 1
+
 
 @dataclass
 class Classification:
@@ -18,6 +21,7 @@ class Classification:
     result: str
     # other fields here
 
+
 @app.route("/upload", methods=["POST"])
 def Upload():
     if "audio" not in request.files:
@@ -25,7 +29,7 @@ def Upload():
 
     audio_file = request.files["audio"]
     if audio_file.filename:
-        uploads_path = 'tmp/uploads'
+        uploads_path = "tmp/uploads"
         Path(uploads_path).mkdir(parents=True, exist_ok=True)
 
         uploads = Path(uploads_path)
@@ -33,19 +37,38 @@ def Upload():
         file_path = uploads / secure_filename(audio_file.filename)
         audio_file.save(file_path)
 
-        clf = classify(file_path)
+        wav_file = convertWAV(file_path)
+        clf = classify(wav_file)
 
-        return jsonify({
-            "class": clf.sd.value,
-            "result": clf.result,
-        }), HTTPStatus.OK
-    
-    return jsonify({"error": "There was a problem saving the file"}), HTTPStatus.INTERNAL_SERVER_ERROR
+        return (
+            jsonify(
+                {
+                    "class": clf.sd.value,
+                    "result": clf.result,
+                }
+            ),
+            HTTPStatus.OK,
+        )
+
+    return (
+        jsonify({"error": "There was a problem saving the file"}),
+        HTTPStatus.INTERNAL_SERVER_ERROR,
+    )
+
 
 def classify(audio_path: Path) -> Classification:
     # insert classification logic here
     return Classification(
         sd=SD_Class.NSD,
-        confidence_score=0.826, # 82.6%
-        result="You are not sleep deprived."
+        confidence_score=0.826,  # 82.6%
+        result="You are not sleep deprived.",
     )
+
+
+def convertWAV(audio: Path) -> Path:
+    basename = audio.stem
+    file = AudioSegment.from_file(audio)
+    file.export(basename, format="wav")
+    
+    audio.unlink()
+    return Path(f"{basename}.wav")
