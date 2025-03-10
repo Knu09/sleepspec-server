1106bb745f8772969bcf0e21d0c2af43694fbd58
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-10 13:36:19 +0800

    return defined classification type

--- server.py
+++ server.py
@@ -3,12 +3,19 @@ from werkzeug.utils import secure_filename
 from flask import Flask, request, jsonify
 from pathlib import Path
 from http import HTTPStatus
+from dataclasses import dataclass
 
 app = Flask(__name__)
 
-class Class(Enum):
+class SD_Class(Enum):
     NSD = 0
     SD = 1
+
+@dataclass
+class Classification:
+    sd: SD_Class
+    confidence_score: int
+    # other fields here
 
 @app.route("/", methods=["POST"])
 def Index():
@@ -20,14 +27,15 @@ def Index():
         file_path = Path("./tmp/uploads") / secure_filename(audio_file.filename)
         audio_file.save(file_path)
 
+        classification = classify(file_path)
+
         return jsonify({
-            "class": classify(file_path),
+            "class": classification.sd,
             "result": "You are not sleep deprived.",
         }), HTTPStatus.OK
     
     return jsonify({"error": "There was a problem saving the file"}), HTTPStatus.INTERNAL_SERVER_ERROR
 
-def classify(audio_path: Path) -> Class:
+def classify(audio_path: Path) -> Classification:
     # insert classification logic here
-
-    return Class.NSD # or Class.SD
+    pass
