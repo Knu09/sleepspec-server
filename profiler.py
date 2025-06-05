04fd3e5dd420c6d79af7ee0a0a8995847a5e6cf2
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-06-05 08:24:19 +0800

    add profiler

---
+++ profiler.py
@@ -0,0 +1,10 @@
+import time
+
+def profile(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{func.__name__} took {end - start:.6f} seconds")
+        return result
+    return wrapper
