9c4ee7ef2cc766a3c5207f0c73342f9de1e91525
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-08-20 09:05:46 +0800

    feat (preprocess.py): apply noise reduction using spectral gating
    optional: add a method of background noise removal using fourier transform

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -9,6 +9,7 @@ import shutil
 import matplotlib.pyplot as plt
 import scipy.fftpack as fft
 from scipy.signal import medfilt
+from pedalboard import *
 
 
 def check_audio_extension(input_file: Path):
@@ -42,28 +43,55 @@ def get_unique_output_dir(base_dir: Path) -> Path:
     return output_dir
 
 
-def background_noise_removal(input_file):
+def background_noise_removal(y, sr):
     """
     Remove background noise segments from the audio file.
     Uses Fourier Transform.
     """
 
-    y, sr = librosa.load(input_file, sr=None)
+    # y, sr = librosa.load(input_file, sr=None)
 
     # Compute STFT
     S_full, phase = librosa.magphase(librosa.stft(y))
 
+    # Use first 10% of frames to estimate noise profile
+    num_frames = S_full.shape[1]
+    noise_frames = int(num_frames * 0.1)
+
     # Estimate noise profile as the median across time
-    noise_power = np.mean(S_full[:, int(sr * 0.1)], axis=1)
+    noise_power = np.mean(S_full[:, :noise_frames], axis=1)
 
     # Build mask
     mask = S_full > noise_power[:, None]
     mask = mask.astype(float)
     mask = medfilt(mask, kernel_size=(1, 5))
+
+    # Apply mask
     S_clean = S_full * mask
     y_clean = librosa.istft(S_clean * phase)
 
     return y_clean
+
+
+def noise_reduction(y, sr, stationary=False, prop_decrease=0.75):
+
+    # Spectral gaiting noise reduction
+    reduced_noise = nr.reduce_noise(
+        y=y, sr=sr, stationary=stationary, prop_decrease=prop_decrease
+    )
+
+    # Apply effects chain (Noise Gate, Compressor, EQ, Gain)
+    board = Pedalboard(
+        [
+            NoiseGate(threshold_db=30, ratio=1.5, release_ms=250),
+            Compressor(threshold_db=16, ratio=2.5),
+            LowShelfFilter(cutoff_frequency_hz=400, gain_db=10, q=1),
+            Gain(gain_db=10),
+        ]
+    )
+
+    effected = board(reduced_noise, sr)
+    return effected
 
 
 # Define preprocessing function
@@ -105,14 +133,6 @@ def preprocess_audio(
     # Load and resample audio
     y, sr = load_audio_with_soundfile(input_file)
 
-    # Apply background noise removal
-    # y = background_noise_removal(input_file)
-
-    # Check if noise removal is active in client-side
-    if noise_removal_flag:
-        # Apply noise reduction using spectral gating
-        y = nr.reduce_noise(y=y, sr=sr)
-
     # Noise reduction in chunks (if audio is long)
     # chunk_size = sr * 5  # Process 5-second chunks
     # y_denoised = np.concatenate(
@@ -129,6 +149,14 @@ def preprocess_audio(
     if sr != target_sr:
         y = librosa.resample(y, orig_sr=sr, target_sr=target_sr)
         sr = target_sr
+
+    # Check if noise removal is active in client-side
+    if noise_removal_flag:
+        # Apply background noise removal using fourier transform
+        # y = background_noise_removal(y, sr)
+
+        # Apply noise reduction using spectral gating
+        y = noise_reduction(y, sr, stationary=True, prop_decrease=0.75)
 
     # total_samples = len(y_denoised)
 
