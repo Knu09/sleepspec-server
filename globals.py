aba233915fb4a8519247a1f08333dc9f71b9f11e
GizakiF <aranetagian4221@gmail.com>
2025-11-06 15:16:20 +0800

    added a "2" string should the max workers be "None" to prevent errors in starting up

--- globals.py
+++ globals.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 
-MAX_WORKERS = int(os.getenv("MAX_WORKERS")) or 2
+MAX_WORKERS = int(os.getenv("MAX_WORKERS") or "2") or 2
 OUTDIR = Path(os.getenv("OUTDIR") or "/tmp/sleepspec")
 OUTDIR.mkdir(exist_ok=True)
